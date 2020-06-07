4f83e375739b9192c11a768a9ddaacd11b7adbda
Ashish138200 <53967293+Ashish138200@users.noreply.github.com>
2020-06-07 19:03:44 +0530

    String Formatting

---
+++ MainCode/StringFormatting.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+surname = input("Enter your surname: ")
+message = "Hello %s %s" %(name,surname)
+message1 = f"Hello {name} {surname}"
+message2 = f"Hello {name} {surname}. What's up? "
+print(message)
+print(message1)
+print(message2)
